3a1592e6e6e7796d3461e84d35f4548f939dfd0b
Golli Bala Bhargavi <balabhargavi28@gmail.com>
2026-05-03 21:13:16 +0530

    Add files via upload

---
+++ Notebooks/custom_utils.py
@@ -0,0 +1,27 @@
+from typing import List
+from pyspark.sql import DataFrame
+from pyspark.sql.window import Window
+
+class transformations:
+    def dedup(self, df: DataFrame, dedup_cols: List, cdc:str):
+     
+      df=df.withColumn("dedupKey",concat(*dedup_cols))
+      df=df.withColumn("dedupCounts",row_number().over(Window.partitionBy("dedupKey").orderBy(desc(cdc))))
+      df=df.filter(col('dedupCounts')==1)
+      df=df.drop("dedupKey","dedupCounts")
+
+      return df
+
+    def process_timestamp(self, df):
+      df=df.withColumn("process_timestamp",current_timestamp())
+      return df
+    
+    def upsert(self,df,key_cols):
+      merge_condition="AND".join([f"src.{i}=tgt.{i}" for i in key_cols])
+      dlt_obj = DeltaTable.forName("pyspark_dbt.silver.{table}")
+      dlt_obj.alias("trg").merge(df.alias("src"),merge_condition)\
+        .whenMatchedUpdateAll(condition="src.{cdc}>=trg.{cdc}")\
+          .whenNotMatchedInsertAll()\
+            .execute()
+      return 1
+      return df
